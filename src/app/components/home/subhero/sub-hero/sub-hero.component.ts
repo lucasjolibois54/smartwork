60b1078ffa59cf47bd6e0aa202d014cc94618221
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-05 20:36:20 +0200

    Create sub-hero.component.ts

---
+++ src/app/components/home/subhero/sub-hero/sub-hero.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'home-sub-hero',
+  templateUrl: './sub-hero.component.html',
+  styleUrls: ['./sub-hero.component.scss'],
+})
+export class SubHeroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
