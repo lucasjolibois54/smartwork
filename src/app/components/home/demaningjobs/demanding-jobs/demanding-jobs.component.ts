1cf0e8132b76f5f90c0eb9f009673dfb9301a623
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-16 10:34:48 +0200

    Create demanding-jobs.component.ts

---
+++ src/app/components/home/demaningjobs/demanding-jobs/demanding-jobs.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demanding-jobs',
+  templateUrl: './demanding-jobs.component.html',
+  styleUrls: ['./demanding-jobs.component.scss'],
+})
+export class DemandingJobsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
