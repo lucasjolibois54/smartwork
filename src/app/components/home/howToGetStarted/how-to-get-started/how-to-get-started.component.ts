6f1beaeb199c58bed359a8edf3850c428ccbb17f
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-16 10:58:18 +0200

    Create how-to-get-started.component.ts

---
+++ src/app/components/home/howToGetStarted/how-to-get-started/how-to-get-started.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-how-to-get-started',
+  templateUrl: './how-to-get-started.component.html',
+  styleUrls: ['./how-to-get-started.component.scss'],
+})
+export class HowToGetStartedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
